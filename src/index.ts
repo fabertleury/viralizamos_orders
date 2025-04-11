cc2ec34a141db241d37542bac27ba02fb3c84b29
fabertleury <fabert_@hotmail.com>
2025-04-11 19:44:11 -0300

    Ajuste

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Este é o ponto de entrada principal do aplicativo
+// Importar o servidor e iniciar
+
+import './server';
+
+// Exportar qualquer coisa que possa ser útil para outros módulos
+export * from './graphql/schemas';
+export * from './graphql/resolvers';
